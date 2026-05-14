fc5611f1384db498f360b55f698dd240eecf6471
Karamjk <87203495+Karamjk@users.noreply.github.com>
2026-05-13 20:42:46 -0400

    Add files via upload

---
+++ display.py
@@ -0,0 +1,7 @@
+print("hello code number 2 ")
+print("hello day 2 of waiting untill i do roblox studio ")
+print("day 13 of waiting until my birthday may 30th")
+print("1-2-3-4-5-6-7-8-9-10-11-12-13-14-15-16-17-18-19-20-21-22-23-24-25-26-27-'28-29-30-31-32-33-34-35-36-37-38-38-39-40-hi i am a small youtuber and like poppy playtime minecraft and coding and making games and play ta",4)
+a=2
+e=3648654789
+print("a")
