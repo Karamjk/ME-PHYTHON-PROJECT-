3e9041ab7c4afafe0755adb49c53f438a02ab4f9
Karamjk <87203495+Karamjk@users.noreply.github.com>
2026-07-01 20:32:49 -0400

    Add character counting functionality in hjghdkg.py

---
+++ hjghdkg.py
@@ -0,0 +1,12 @@
+string=input("bob: ")
+print(string)
+char = input("bob: ")
+print(char)
+i = 0
+count = 0
+while i < len(string):
+    if string[i] == char:
+        count += 1
+    i += 1
+print(count)
+print("The character", char, "appears", count, "times in the string.")
